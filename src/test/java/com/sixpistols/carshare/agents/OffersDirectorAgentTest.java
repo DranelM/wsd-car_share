1b616a8182e4071ca313b94b270f5670cfe929c9
S.Szymborski <s.szymborski@stud.elka.pw.edu.pl>
2019-11-29 16:50:28 +0100

    Add sample JUnit tests

---
+++ src/test/java/com/sixpistols/carshare/agents/OffersDirectorAgentTest.java
@@ -0,0 +1,18 @@
+package com.sixpistols.carshare.agents;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OffersDirectorAgentTest {
+
+    @Test
+    void setup() {
+        // TODO
+    }
+
+    @Test
+    void takeDown() {
+        // TODO
+    }
+}
